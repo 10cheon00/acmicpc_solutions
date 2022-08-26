9d70231ac57fc0d61b542b5974663d4d1db5a39b
10cheon00 <10cheon00@gmail.com>
2022-08-26 22:56:19 +0900

    solved: 1374

--- acmicpc_solutions/1374.cpp
+++ acmicpc_solutions/1374.cpp
@@ -1,3 +1,11 @@
+/*
+ * 1374 강의실
+ * 골드V
+ */
+//시작 시간이 빠른 순부터
+//같은 시작 시간일 경우 종료 시간이 빠른 순부터
+//종료 시간이 제일 빠른 곳에 삽입
+//종료 시간이 제일 빠른 곳을 찾기 위해 우선순위 큐를 사용
 #include <algorithm>
 #include <iostream>
 #include <queue>
@@ -11,52 +19,22 @@ bool compare(Lecture &l1, Lecture &l2) {
   }
   return l1.start < l2.start;
 }
+
 int main() {
-  int N, c, size = 1, room[100001]{0};
+  int N, c;
   Lecture lec[100000];
   scanf("%d", &N);
   for (int i = 0; i < N; i++) {
     scanf("%d %d %d", &c, &lec[i].start, &lec[i].end);
   }
-  std::sort(lec, lec + N, compare);
-
+  sort(lec, lec + N, compare);
+  priority_queue<int, vector<int>, greater<int>> room;
+  room.push(0);
   for (int i = 0; i < N; i++) { // lec
-    int j = 1;
-    for (; j <= size; j++) { // room
-      if (room[j] <= lec[i].start) {
-        room[j] = lec[i].end;
-        break;
-      }
+    if (lec[i].start >= room.top()) {
+      room.pop();
     }
-    if (j > size) {
-      room[++size] = lec[i].end;
-    }
+    room.push(lec[i].end);
   }
-  printf("%d", size);
+  printf("%d", room.size());
 }
-/*
-시작 시간이 빠른 순부터
-같은 시작 시간일 경우 종료 시간이 빠른 순부터
-01   5    1         2
-  +------------
-   +-----
-      +--------------
-      +---------------------
-       +------
-            +------
-                +-----
-                    +-----
-
-
-10
-10 15 20
-2 0 5
-3 5 7
-1 0 4
-8 12 15
-4 5 9
-7 10 11
-5 7 12
-6 9 12
-9 12 17
-*/
